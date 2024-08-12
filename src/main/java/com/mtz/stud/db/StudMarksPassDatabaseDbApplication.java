efec2dc4e0b5312f8066a7ee0385e39ceb0ba26f
karthik polisetty <karthikpolisetty1315@gmail.com>
2024-08-12 19:36:41 +0530

    new data base

---
+++ src/main/java/com/mtz/stud/db/StudMarksPassDatabaseDbApplication.java
@@ -0,0 +1,13 @@
+package com.mtz.stud.db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudMarksPassDatabaseDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudMarksPassDatabaseDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mtz/stud/db/StudMarksPassDatabaseDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mtz.stud.db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudMarksPassDatabaseDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
